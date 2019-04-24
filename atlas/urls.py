0027f1e773501c4154ec0147b38e4e2005b85f0d
Doug Newman <doug@dougnewman.org>
2019-04-24 02:39:36 -0400

    Initial database and URL setup

---
+++ atlas/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from atlas import views
+
+urlpatterns = [
+
+]
--- historicshipsatlas2/settings.py
+++ historicshipsatlas2/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    # Register atlas
+    'atlas.apps.AtlasConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +78,14 @@ WSGI_APPLICATION = 'historicshipsatlas2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'historicshipsatlas2',
+        'USER': 'atlas',
+        'PASSWORD': 'HSApw0419!',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
